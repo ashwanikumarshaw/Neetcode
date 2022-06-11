0070be35575300d4db78f315fc8dd090a2fbee8b
Ashwanikumarshaw <Ashwanikumar11shaw@gmail.com>
2022-06-12 01:03:57 +0530

    twoSum

--- src/ArrayAndHashing/TwoSum_1.java
+++ src/ArrayAndHashing/TwoSum_1.java
@@ -1,5 +1,6 @@
 package ArrayAndHashing;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,7 +26,7 @@ public class TwoSum_1 {
     }
 
     public static void main(String[] args) {
-        System.out.println(twoSum(new int[] {2,7,11,15},9));
+        System.out.println(Arrays.toString( twoSum(new int[] {2,7,11,15},9)));
     }
 
 }
