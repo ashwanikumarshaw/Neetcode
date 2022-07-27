8a51dd25895bbfc8de75e3a73134205ea6ca6062
Ashwanikumarshaw <Ashwanikumar11shaw@gmail.com>
2022-07-27 10:44:39 +0530

    ListNode

--- src/LinkedList/ReverseLinkedList_206.java
+++ src/LinkedList/ReverseLinkedList_206.java
@@ -50,19 +50,19 @@ public class ReverseLinkedList_206 {
 
 }
 
-class ListNode {
-	int val;
-	ListNode next;
-
-	ListNode() {
-	}
-
-	ListNode(int val) {
-		this.val = val;
-	}
-
-	ListNode(int val, ListNode next) {
-		this.val = val;
-		this.next = next;
-	}
-}
+//class ListNode {
+//	int val;
+//	ListNode next;
+//
+//	ListNode() {
+//	}
+//
+//	ListNode(int val) {
+//		this.val = val;
+//	}
+//
+//	ListNode(int val, ListNode next) {
+//		this.val = val;
+//		this.next = next;
+//	}
+//}
